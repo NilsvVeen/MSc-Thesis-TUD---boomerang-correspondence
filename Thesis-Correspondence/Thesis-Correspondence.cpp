7b2576da155215a12a2fd34864e8c251f759ba3d
Nils <47635432+HUILIANGAA@users.noreply.github.com>
2024-09-03 13:31:22 +0100

    setup cmake and cpp

---
+++ Thesis-Correspondence/Thesis-Correspondence.cpp
@@ -0,0 +1,12 @@
+﻿// Thesis-Correspondence.cpp : Defines the entry point for the application.
+//
+
+#include "Thesis-Correspondence.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}

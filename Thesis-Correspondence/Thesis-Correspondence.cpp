ec95e2a15876edae8f1f7ceb2fb3f319c56b33a5
Nils <nilsvanveen00@gmail.com>
2025-04-15 10:53:39 +0200

    add models

--- Thesis-Correspondence/Thesis-Correspondence.cpp
+++ Thesis-Correspondence/Thesis-Correspondence.cpp
@@ -109,7 +109,8 @@ int main2()
         //std::string modelPath = GLOBAL_MODELS_DIRECTORY + "/Boomerang_12_decimate01.stl";
         //std::string modelPath = GLOBAL_MODELS_DIRECTORY + "/Boomerang_12_decimate01.stl";
         //std::string modelPath = GLOBAL_MODELS_DIRECTORY + "/Deborah.stl";
-        std::string modelPath = GLOBAL_MODELS_DIRECTORY + "/Boomerang_11_decimate01.stl";
+        //std::string modelPath = GLOBAL_MODELS_DIRECTORY + "/Boomerang_11_decimate01.stl";
+        std::string modelPath = GLOBAL_MODELS_DIRECTORY + "/Boomerang_24_decimate01.stl";
 
         // Call the function to view the STL object
         //viewSTLObject(modelPath);
